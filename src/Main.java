46afb912b5fbb7d6f0305e47aae584db97d710f7
AnTonyHa <toan.ha1896@gmail.com>
2020-11-14 23:39:04 -0800

    Set-up project's repository

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.math.BigInteger;
+
+/**
+ * CECS 328 PA5: hobbits
+ *
+ * @author Tony Ha
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
