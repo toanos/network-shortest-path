199ee93d993be1a5886e345c805df25a34641bf9
AnTonyHa <toan.ha1896@gmail.com>
2020-11-27 14:05:17 -0800

    Implement floating pad class

---
+++ src/FloatingPads.java
@@ -0,0 +1,17 @@
+/**
+ * This class represents the floating pads.
+ *
+ * @author Tony Ha
+ */
+public class FloatingPads {
+
+    public FloatingPads() {
+        // TODO Implement default constructor
+    }
+
+    /** Return number of existing floating pads */
+    public static int getNumPads() { return numPads; }
+
+    private static int numPads = 0;
+
+}
