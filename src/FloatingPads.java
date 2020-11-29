ca34e8002734bcd9a3b049b33de4882a6e6c9987
AnTonyHa <toan.ha1896@gmail.com>
2020-11-29 10:31:23 -0800

    Implement graph structure for floating pads

--- src/FloatingPads.java
+++ src/FloatingPads.java
@@ -1,3 +1,6 @@
+import java.math.BigInteger;
+import java.util.LinkedList;
+
 /**
  * This class represents the floating pads.
  *
@@ -9,6 +12,10 @@ public class FloatingPads {
         // TODO Implement default constructor
     }
 
+    public static void connectPads(FloatingPads u, FloatingPads v) {
+        // TODO Is this the right way to implement an adjacency list? Or should I go with adjacency matrix?
+    }
+
     /**
      * Return number of existing floating pads
      */
@@ -17,6 +24,7 @@ public class FloatingPads {
     }
 
     private static int numPads = 0;
-    private int label;
+    private BigInteger label;
+    private LinkedList<FloatingPads> neighbors;
 
 }
