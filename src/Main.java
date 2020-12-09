c10287aa0d638a974918b715c223b4dd0539427d
AnTonyHa <toan.ha1896@gmail.com>
2020-12-08 18:56:32 -0800

    Add new input file

--- src/Main.java
+++ src/Main.java
@@ -18,7 +18,7 @@ public class Main {
         ArrayList<String> hoppingPaths = new ArrayList<>();
         // Input phase
         try {
-            Scanner inFile = new Scanner(new File("input0.txt"));
+            Scanner inFile = new Scanner(new File("input2.txt"));
             ArrayList<BigInteger> inputPads = new ArrayList<>();
             do {
                 String line = inFile.nextLine();
